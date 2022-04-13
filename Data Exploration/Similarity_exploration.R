e5827092f074cbec18bd94ee0939919e4660756d
Marit Verbruggen <m.verbruggen_2@tilburguniversity.edu>
2022-04-13 14:00:00 +0200

    add descriptive statistics for similar data sets

--- Data Exploration/Similarity_exploration.R
+++ Data Exploration/Similarity_exploration.R
@@ -13,6 +13,7 @@ View(similar_meta)
 tabel <- as.data.frame(table(similar_map$focal_book_id))
 View(tabel)
 tabel <- table(tabel$Freq)
+View(tabel)
 tabel[1]
 tabel[1] / (tabel[1]+tabel[2]+tabel[3]+tabel[4]+tabel[5])
 
@@ -22,3 +23,25 @@ length(df_uniq)
 
 df_uniq <- unique(similar_meta$similar_book_id)
 length(df_uniq)
+View(similar_map)
+View(similar_meta)
+
+#check whether NAs inserted
+sum(is.na(similar_meta))
+sum(is.na(similar_meta$publication_year))
+
+#descriptive statistics
+mean(similar_meta$rating)
+sd(similar_meta$rating)
+min(similar_meta$rating)
+max(similar_meta$rating)
+
+mean(similar_meta$reviews)
+sd(similar_meta$reviews)
+min(similar_meta$reviews)
+max(similar_meta$reviews)
+
+mean(similar_meta$average.rating)
+sd(similar_meta$average.rating)
+min(similar_meta$average.rating)
+max(similar_meta$average.rating)

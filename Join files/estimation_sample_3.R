0467104043f9bbb6e7f1f1d0d767fbac7bc0e2dd
Marit Verbruggen <m.verbruggen_2@tilburguniversity.edu>
2022-04-19 15:33:21 +0200

    create estimation sample 3

--- Join files/estimation_sample_3.R
+++ Join files/estimation_sample_3.R
@@ -8,7 +8,6 @@ similar_meta <- read.csv("../Datasets/similar_meta_thesis.csv")
 giveways <- read.csv("../Datasets/giveaways_thesis.csv")
 book_info <- read.csv("../Datasets/book_df_cleanPublisher.csv")
 
-
 #delete more than 1 similar books 
 similar_map <-similar_map[!duplicated(similar_map$focal_book_id), ] 
 View(similar_map)
@@ -18,25 +17,18 @@ similar <- similar_map %>% inner_join(similar_meta, by="similar_book_id")
 View(similar)
 
 #delete variables not used in further analysis
-similar <- subset(similar, select=c(focal_book_id, similar_book_id, reviews, rating, average.rating, publication_date))
+similar <- similar %>% select(focal_book_id, title=title.x, reviews, rating, average.rating, publication_date)
+similar$treated <- "0"
 
 #create subset of book_info data set 
-book <- subset(book_info, select = c(id, ratings_count, text_reviews_count, average_rating,book_publication_date))
+book <- book_info %>% select(focal_book_id=id, title, reviews=text_reviews_count, rating=ratings_count, average.rating=average_rating, publication_date=book_publication_date)
+book$treated <- "1"
 
-#join focal books to information in book data set 
-focal_books <- subset(similar, select=focal_book_id)
-focal_books_meta <- focal_books %>% inner_join(book, by=c("focal_book_id"="id"))
+#join data sets 
+book <- filter(book, focal_book_id %in% similar$focal_book_id)
 
-
-#create subset of similar books and match with focal book file 
-similar_sub <- subset(similar, select=c(focal_book_id, similar_book_id))
-focal_books <- similar_sub %>% inner_join(focal_books_meta, by="focal_book_id")
-
-
-
-names(f)[1] <- 'P_Class'
-
+est_3 <- rbind(book,similar)
+View(est_3)
 
 #save the data 
-fwrite(similar, "../Estimation_samples/similar_books.csv")
-fwrite(focal_books_meta, '../Estimation_samples/focal_books.csv')
+fwrite(est_3, "../Estimation_samples/similar_books.csv")
